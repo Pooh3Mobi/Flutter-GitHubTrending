b80232cb2f7bb5fad85f41680afea465c1a1d21a
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-03-11 05:21:56 +0900

    load rss data and show

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:feedparser/feedparser.dart';
+//import 'package:url_launcher/url_launcher.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -31,9 +37,67 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
-}
 
+}
+class FeedStatus{
+  FeedStatus(this.feed, this.msg);
+  final List<Entry> feed;
+  final String msg;
+}
 class _MyHomePageState extends State<MyHomePage> {
+  var _result = new FeedStatus(null, "Loading");
+
+  _MyHomePageState() {
+    _getFeed();
+  }
+
+  _getFeed() async {
+    var url = 'http://github-trends.ryotarai.info/rss/github_trends_java_daily.rss';
+    var httpClient = new HttpClient();
+
+    FeedStatus result;
+    try {
+      setState(() {
+        _result = new FeedStatus(null, "Loading");
+      });
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.OK) {
+        var rss = await response.transform(UTF8.decoder).join();
+        Feed f = parse(rss, strict: false);
+        List<FeedItem> items = f.items;
+        List<Entry> entries = items
+            .map(
+                (i) => new Entry(
+                i.title.split(" ")[0],
+                i.description,
+                i.link
+            ))
+            .toList();
+        result = new FeedStatus(entries, null);
+      } else {
+        result = new FeedStatus(
+            null,
+            'Error getting rss feed:\nHttp status ${response.statusCode}'
+        );
+      }
+    } catch (exception) {
+      result = new FeedStatus(
+          null,
+          'Failed getting rss feed:\n' + exception.toString()
+      );
+    }
+
+    print(result);
+    // If the widget was removed from the tree while the message was in flight,
+    // we want to discard the reply rather than calling setState to update our
+    // non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _result = result;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,9 +111,25 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body:  new ListView.builder(
-        itemBuilder: (BuildContext context, int index) => new EntryItem(data[index], index),
-        itemCount: data.length,
+      body: _result.msg != null ?
+      new Center(
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  <Widget>[
+              new Text(
+                  _result.msg,
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(
+                    fontFamily: "Rock Salt",
+                    fontSize: 17.0,
+                  )
+              )
+            ]
+        ),
+      ) :
+      new ListView.builder(
+        itemBuilder: (BuildContext context, int index) => new EntryItem(_result.feed[index], index),
+        itemCount: _result.feed.length,
       ),
     );
   }
@@ -58,11 +138,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
 // One entry in the multilevel list displayed by this app.
 class Entry {
-  Entry(this.title);
+  Entry(this.title, this.description, this.link);
   final String title;
+  final String description;
+  final String link;
 }
-final List<Entry> data  = new List<Entry>
-    .generate(100, (int index) => new Entry('Entry $index'));
 
 class EntryItem extends StatelessWidget {
   const EntryItem(this.entry, this.position);
@@ -72,29 +152,28 @@ class EntryItem extends StatelessWidget {
 
   Widget _buildTiles(Entry root, BuildContext context) {
     return new Container(
-      height: 100.0,
+      height: 160.0,
       color: const Color(0x08000000),
       padding: new EdgeInsets.fromLTRB(4.0, 2.0, 4.0, 2.0),
-        child: new Card(
-          color: Colors.white,
-          child: new ListTile(
-            leading: const Icon(Icons.account_circle),
-            key: new PageStorageKey<Entry>(root),
-            title: new Text(root.title),
-            subtitle: new Container(
-                padding: new EdgeInsets.fromLTRB(16.0, 16.0, 4.0, 18.0),
-                child: new Text("#$position")
-            ),
-            trailing: const Icon(Icons.arrow_forward_ios),
-
-            onTap: () {
-              Scaffold.of(context).showSnackBar(new SnackBar(
-                content: new Text("You tapped # $position"),
-              ));
-            },
+      child: new Card(
+        color: Colors.white,
+        child: new ListTile(
+//            leading: const Icon(Icons.account_circle),
+          key: new PageStorageKey<Entry>(root),
+          title: new Text("No.${position +1} ${root.title}"),
+          subtitle: new Container(
+              padding: new EdgeInsets.fromLTRB(16.0, 16.0, 4.0, 18.0),
+              child: new Text("description:\n${root.description}")
           ),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () {
+//              _launchURL(root.link);
+            Scaffold.of(context).showSnackBar(new SnackBar(
+              content: new Text("You tapped # $position"),
+            ));
+          },
         ),
-
+      ),
     );
   }
 
@@ -102,4 +181,12 @@ class EntryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return _buildTiles(entry, context);
   }
+
+//  _launchURL(String url) async {
+//    if (await canLaunch(url)) {
+//      await launch(url);
+//    } else {
+//      throw 'Could not launch $url';
+//    }
+//  }
 }
